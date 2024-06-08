ed2ba3f2323e79328c6de04d394fb1a08d8b5d11
Tempozya <timo.semenow@yandex.ru>
2024-06-08 20:39:06 +0300

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    data = {
+        'title': 'Соседи',
+
+    }
+    return render(request, 'main/index.html',data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
