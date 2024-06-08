45aaac47129384427675b82d1d6edd77e241d93b
Tempozya <timo.semenow@yandex.ru>
2024-06-08 22:20:42 +0300

    Добавлены новости

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ from main import views
 urlpatterns = [
     path('', views.index,name='home'),
     path('about', views.about,name='about'),
+    path('rooms', views.rooms,name='rooms'),
+    path('services', views.services,name='services'),
+    path('contacs', views.contacs,name='contacs'),
+    path('news', views.news,name='news'),
 ]
--- main/views.py
+++ main/views.py
@@ -7,8 +7,19 @@ def index(request):
         'title': 'Соседи',
 
     }
-    return render(request, 'main/index.html',data)
-
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
+
+def rooms(request):
+    return render(request, 'main/rooms.html')
+
+def services(request):
+    return render(request, 'main/amenities.html')
+
+def contacs(request):
+    return render(request, 'main/contact.html')
+
+def news(request):
+    return render(request, 'main/news.html')
