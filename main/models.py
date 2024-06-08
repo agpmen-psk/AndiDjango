e188d58961fb1c5dd9a56696b4643de1e901df69
Tempozya <timo.semenow@yandex.ru>
2024-06-08 23:04:27 +0300

    Реализовал БД на sqllite Добавил таблицу новости Вывожу новости циклом

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=100)
+    preview = models.CharField('Анонс', max_length=250)
+    content = models.TextField('Текст статьи')
+    pub_date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- main/views.py
+++ main/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Articles
 
 
 def index(request):
     data = {
         'title': 'Соседи',
-
     }
     return render(request, 'main/index.html', data)
+
 
 def about(request):
     return render(request, 'main/about.html')
 
+
 def rooms(request):
     return render(request, 'main/rooms.html')
+
 
 def services(request):
     return render(request, 'main/amenities.html')
 
+
 def contacs(request):
     return render(request, 'main/contact.html')
 
+
 def news(request):
-    return render(request, 'main/news.html')
+    news = Articles.objects.all()
+    return render(request, 'main/news.html', {'news': news})
